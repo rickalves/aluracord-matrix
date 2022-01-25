f4822e3c949613fcdf6302a8b36ad8c93bd540b4
Rick_Alves <henryworkdev.ti@gmail.com>
2022-01-25 16:26:35 -0300

    change in the home page

---
+++ pages/chat.js
@@ -0,0 +1,5 @@
+export default function ChatPage(props){
+    return(
+        <h1>Bem vindo ao Chat!</h1>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,8 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router'
+
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -46,30 +20,18 @@ function Titulo(props) {
   );
 }
 
-// Componente React
-// function HomePage() {
-//     // JSX
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     )
-// }
-// export default HomePage
-
 export default function PaginaInicial() {
-  const username = 'rickalves';
+
+  const [username, setUsername] = useState('rickalves')
+  const router = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          // backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundImage: 'url(https://images5.alphacoders.com/672/thumb-1920-672012.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
@@ -82,26 +44,40 @@ export default function PaginaInicial() {
               xs: 'column',
               sm: 'row',
             },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
-            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+            width: '100%', maxWidth: '600px',
+            borderRadius: '20px 0px 20px 0', padding: '32px', margin: '16px',
+            boxShadow: '0 2px 10px 5px #018E42',
             backgroundColor: appConfig.theme.colors.neutrals[700],
+            opacity: '0.9'
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (e) {
+              e.preventDefault()
+              router.push('/chat')
+            }}
+
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Text variant="body3" 
+              styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
-
             <TextField
+              value={username}  
+              onChange={(e) => {
+                //Desafio proposto pelo Mario
+                setUsername() //limpa o estado username
+                const valor = e.target.value //armazena o valor em uma constante
+                if(valor.length > 2) //testa se quantidade de caracteres do user é maior que 2
+                    setUsername(valor) //atribui o username
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -138,7 +114,7 @@ export default function PaginaInicial() {
               backgroundColor: appConfig.theme.colors.neutrals[800],
               border: '1px solid',
               borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: '10px',
+              borderRadius: '20px 0 20px 0',
               flex: 1,
               minHeight: '240px',
             }}
